bf9f9a29abed4d056bc87f8fa4680759c7dca257
Drake Gens <drake.gens@gmail.com>
2017-03-25 18:25:46 -0500

    can add multiple items (at least in appearance)

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
-
-  // Place JavaScript code here...
-
-  //handling add new item to registry button
-  function addAnotherItemForm(){
-    return;
-  }
-
+ //handling add new item to registry button
+  $("#a").click(function addAnotherItemForm(){
+    console.log("trying to invoke");
+    $('#listOfItems').append(`<div class="registry-item"><div class="form-group">
+  <label class="col-sm-3 control-label" for="nameOfItem">Item Name</label>
+  <div class="col-sm-7">
+    <input class="form-control" type="text" name="nameOfItem" id="nameOfItem"/>
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-sm-3 control-label" for="itemPrice">Price ($)</label>
+  <div class="col-sm-7">
+    <input class="form-control" type="number" name="itemPrice" id="itemPrice"/>
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-sm-3 control-label" for="itemDescription">Description</label>
+  <div class="col-sm-7">
+    <input class="form-control" type="text" name="itemDescription" id="itemDescription"/>
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-sm-3 control-label" for="itemURL">URL (Optional)</label>
+  <div class="col-sm-7">
+    <input class="form-control" type="text" name="itemURL" id="itemURL"/>
+</div>
+  </div>
+</div>`);
+  })
 });
