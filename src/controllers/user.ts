327efee990727725e9a00ac7b19dd98cad700c56
Drake Gens <drake.gens@gmail.com>
2017-03-25 16:49:44 -0500

    name will be included with acct creation

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -90,7 +90,10 @@ export const postSignup = (req, res, next) => {
 
   const user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: req.body.password,
+    profile: {
+      name: req.body.name
+    }
   });
 
   User.findOne({ email: req.body.email }, (err, existingUser) => {
