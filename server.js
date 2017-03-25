540c8a686f6150f2c67b0ebb428c6507990c77c8
Stephen Reddekopp <stephen.reddekopp@smrxt.com>
2017-03-25 09:40:34 -0500

    copy files from hackathon starter

---
+++ server.js
@@ -0,0 +1,13 @@
+require('source-map-support').install();
+const chalk = require('chalk');
+
+const app = require('./src/app').App;
+
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
+});
