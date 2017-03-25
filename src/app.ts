bd707d6a2b2f1c24ef78f29b54730fd6d60525e6
Stephen Reddekopp <stephen.reddekopp@smrxt.com>
2017-03-25 12:57:17 -0500

    basic searching by name

--- src/app.ts
+++ src/app.ts
@@ -138,6 +138,7 @@ app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userControl
 
 //registry
 app.get('/registry/new', passportConfig.isAuthenticated, registryController.getNewRegistry)
+app.get('/registry/search', registryController.getSearchRegistry)
 
 /**
  * API examples routes.
--- src/controllers/registry.ts
+++ src/controllers/registry.ts
@@ -1,10 +1,25 @@
 // import * as bluebird from 'bluebird'
 // import Registry from '../models/Registry'
+import User from '../models/User'
 
 // const passport = require('passport')
 
 export const getNewRegistry = (req, res) => {
     res.render('registry/new', {
         title: 'New Registy',
+    })
+}
+
+export const getSearchRegistry = (req, res) => {
+    const userRegex = new RegExp(`.*${req.query.search}.*`, 'i')
+    User.find({ 'profile.name': userRegex }, (err, users) => {
+        if (err) {
+            users = []
+        }
+        res.render(`registry/search`, {
+            title: 'Find Registry',
+            search: req.query.search,
+            users: users,
+        })
     })
 }
