3a513bd16da213f3848bdcee3cf9276f1902bcfc
Stephen Reddekopp <stephen.reddekopp@smrxt.com>
2017-03-25 18:04:08 -0500

    hack way of knowing if a contribution was successful

--- src/controllers/registry.ts
+++ src/controllers/registry.ts
@@ -1,8 +1,7 @@
 // import * as bluebird from 'bluebird'
-import { Registry, Item } from '../models/Registry'
+import { Registry, Item, Contribution } from '../models/Registry'
 import User from '../models/User'
-
-// const passport = require('passport')
+const _ = require('lodash')
 
 export const getNewRegistry = (req, res) => {
     res.render('registry/new', {
@@ -71,13 +70,52 @@ export const getUserRegistry = (req, res) => {
     })
 }
 
-export const  getContribute = (req, res) => {
+export const getContribute = (req, res) => {
     Item.findById(req.params.itemId)
-        .populate('contributions')
+        .populate({
+            path:'contributions',
+            model: 'Contribution',
+        })
         .exec((err, item) => {
             res.render('registry/contribute', {
                 title: 'Contribute',
                 item: item,
             })
         })
+}
+
+export const getContributeSuccess = (req, res) => {
+    const amount = parseInt(req.query.amount, 10) / 100
+    const paymentId = req.query.paymentId
+
+    Contribution.find({ foreignSystemId: paymentId }, (err, contribution) => {
+        Item.findById(req.params.itemId)
+        .populate({
+            path:'contributions',
+            model: 'Contribution',
+        })
+        .exec((err, item) => {
+            if (_.isEmpty(contribution) && req.query.paymentStatus === 'APPROVED') {
+                contribution = new Contribution({
+                    contributorEmail: '',
+                    contributorName: '',
+                    amount,
+                    foreignSystemId: paymentId,
+                })
+                contribution.save()
+                if (_.isNil(item.contributions)) {
+                    item.contributions = [contribution]
+                } else {
+                    item.contributions.push(contribution)
+                }
+
+                item.save()
+            }
+            
+            res.render('registry/contribute_success', {
+                title: 'Successful contribution',
+                item: item,
+            })
+        })
+    })
 }
